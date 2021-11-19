788ae59e23155f644dc83bb7462a78fa94f66be4
Frantss <frantss11@gmail.com>
2021-11-18 21:39:48 -0300

    added experience url

--- src/experience.ts
+++ src/experience.ts
@@ -1,6 +1,7 @@
 export interface Experience {
   place: string;
   position: string;
+  url: string;
   description: string;
   startedAt: Date;
   endedAt?: Date;
@@ -10,6 +11,7 @@ export const experience: Experience[] = [
   {
     place: "ST Consultores",
     position: "Java Jr. Dev",
+    url: "https://www.st.com.uy/",
     description: "Worked with Java and PostgreSQL in an BPM environment",
     startedAt: new Date(2019, 7, 1),
     endedAt: new Date(2019, 11, 31),
@@ -17,6 +19,7 @@ export const experience: Experience[] = [
   {
     place: "Senpai Academi",
     position: "Fullstack Developer Teacher",
+    url: "https://senpaiacademy.com/",
     description: "Teach a course about becoming a fullstack developer",
     startedAt: new Date(2021, 5, 1),
     endedAt: new Date(2021, 10, 31),
@@ -24,12 +27,14 @@ export const experience: Experience[] = [
   {
     place: "Moove-It",
     position: "Fullstack developer",
+    url: "https://moove-it.com/",
     description: "Javascript fullstack developer",
     startedAt: new Date(2020, 2, 10),
   },
   {
     place: "Hulu",
     position: "Consultant",
+    url: "https://www.hulu.com/",
     description:
       "Working as part of the Instrumentation team on behalf of Moove-it",
     startedAt: new Date(2020, 5, 1),
